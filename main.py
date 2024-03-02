f74218094327f7b33b3085bdcb2ac6497682975e
Damian Strzyż <138368043+damianstrzyz@users.noreply.github.com>
2024-03-02 17:50:38 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+import selectorlib
+
+
+URL = 'http://programmer100.pythonanywhere.com/tours/'
+
+def scrape(url):
+    response = requests.get(url)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("webscraping/extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+if __name__ ==  "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
